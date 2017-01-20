248ae9932026f1ecf3d3244d780f0c3c02bc0fc9
Kiril Menshikov <kiril@kiril.me>
2017-01-20 22:46:55 +0200

    final report

--- projects/finding_donors/visuals.py
+++ projects/finding_donors/visuals.py
@@ -70,7 +70,6 @@ def evaluate(results, accuracy, f1):
     for k, learner in enumerate(results.keys()):
         for j, metric in enumerate(['train_time', 'acc_train', 'f_train', 'pred_time', 'acc_test', 'f_test']):
             for i in np.arange(3):
-                
                 # Creative plot code
                 ax[j/3, j%3].bar(i+k*bar_width, results[learner][i][metric], width = bar_width, color = colors[k])
                 ax[j/3, j%3].set_xticks([0.45, 1.45, 2.45])
